63e28ecfc435a1a49217b61ce83dc0ead0f9e8e7
gurubac <gbrbachchu@gmail.com>
2023-01-13 20:38:53 -0800

    Initial Commit

---
+++ AutoUpdateTest/AutoUpdateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoUpdateTestApp.swift
+//  AutoUpdateTest
+//
+//  Created by guru on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AutoUpdateTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
